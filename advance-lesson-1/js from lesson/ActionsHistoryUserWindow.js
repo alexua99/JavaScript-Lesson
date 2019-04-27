0b0e06eb45bbabcd5306a8cfa552b076675ca8ab
Alexander <2356931k@gmail.com>
2019-04-27 20:31:43 +0300

    commit07

---
+++ advance-lesson-1/js from lesson/ActionsHistoryUserWindow.js
@@ -0,0 +1,51 @@
+﻿"use strict";
+
+class ActionsHistoryUserWindow {
+    constructor(windowClassName, url) {
+        this.achievementsUrl = url;
+        this.element = document.querySelector(windowClassName);
+        this.LoadUserAchievements(this.ApplyAchivements, this.element);
+    }
+
+    LoadUserAchievements(funk, element) {
+        $.get(this.achievementsUrl, function (response) {
+            if (response.result) {
+                funk(response.result, element);
+            }
+        }, "json");
+    }
+
+    ApplyAchivements(achievements, element) {
+        let innerHTML = "";
+        for (let achievement of achievements) {
+            let description = "";
+
+            let date = new Date(achievement.createdOn);
+            date = `${date.getDate()}/${date.getMonth()}/${date.getFullYear()}`;
+
+            if (achievement.Description) {
+                description = `<p class="action-description">${achievement.Description}</p>`;
+            }
+            innerHTML += `<div class="history-item">
+                                <span class="action-date mr-2">${date}</span>
+                                <div class="action-photo mr-4">
+                                    <img src="/images/achievement.png" alt="">
+                                </div>
+                                <div class="action-info">
+                                    <span class="action-title">Action title</span>
+                                    ${description}
+                                    <p class="action-nominated">
+                                        Nominated by:
+                                        <a href="#" class="nominated-by">test user</a>
+                                        Approved by:
+                                        <a href="#" class="admin">Admin</a>
+                                    </p>
+                                </div>
+                            </div>`;
+        }
+
+        element.innerHTML = innerHTML;
+    }
+}
+
+new ActionsHistoryUserWindow(".actions-history-wrap", "api/UserAchievement/GetUserAchievements");
---
+++ advance-lesson-1/js from lesson/AuthorizeForm.js
@@ -0,0 +1,91 @@
+﻿'use strict';
+
+//class A {
+//    AAA() {
+//        return 1;
+//    }
+//}
+
+
+//let a = new A();
+//a.AAA();
+
+class AuthorizeForm {
+    constructor(loginUrl, registerUrl) {
+        this.loginUrl = loginUrl;
+        this.registerUrl = registerUrl;
+    }
+
+    Login(email, password) {
+        let data = {
+            email: email,
+            password: password
+        };
+
+        $.post(this.loginUrl, data, function (response) {
+            if (response.redirect) {
+                Alert.Success("You are authorized!");
+                location.href = response.url;
+            }
+            else {
+                Alert.Error("Incorrect email or password");
+            }
+            
+        }, "json");
+    }
+
+    Register(email, password) {
+        let data = {
+            email: email,
+            password: password
+        };
+
+        $.post(this.registerUrl, data, function (response) {
+            if (response.result) {
+                Alert.Success("You are successfuly registered!");
+
+                setTimeout(function () {
+                    location.href = "/";
+                }, 300);
+            }
+            else {
+                Alert.Error(
+                    (response.errors && response.errors.join(" "))
+                    ||
+                    "Unexpected error. Please, try again later."
+                );
+            }
+
+        }, "json");
+    }
+}
+
+let authorizeForm = new AuthorizeForm("/Account/Login", "/Account/Register");
+const bindControls = () => {
+    let loginBtn = document.querySelector("#loginButton");
+    if (loginBtn) {
+        loginBtn.addEventListener("click", function () {
+            let email = document.getElementById("email").value;
+            let password = document.getElementById("password").value;
+
+            authorizeForm.Login(email, password);
+        });
+    }
+
+    let regBtn = document.querySelector("#registerButton");
+    if (regBtn) {
+        regBtn.addEventListener("click", function () {
+            let email = document.getElementById("email").value;
+            let password = document.getElementById("password").value;
+            let confirmPassword = document.getElementById("confirPassword").value;
+
+            //temp decision
+            if (password !== confirmPassword)
+                Alert.Error("Confirmation of password was failed");
+            else
+            authorizeForm.Register(email, password);
+        });
+    }
+};
+
+bindControls();
